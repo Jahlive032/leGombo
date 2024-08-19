24fbf7765eb4d850cd6a9050a745ac4d6ec63019
Jahlive032 <erickoumondji01@gmail.com>
2024-08-19 05:09:13 +0000

    Ajouter une taille eu une largeur à l'image

--- app/(home)/components/CardPresentation.tsx
+++ app/(home)/components/CardPresentation.tsx
@@ -68,8 +68,8 @@ const CardPresentation = () => {
                             src={content[activeSection].imgSrc} 
                             alt={content[activeSection].title} 
                             className="w-full h-auto object-contain"
-                            width={""}
-                            height={""}
+                            width={500}
+                            height={300}
                         />
                     </div>
 
