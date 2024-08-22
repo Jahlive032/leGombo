e3389ff12c80b76a164333f7dec68a96893620fb
Jahlive032 <erickoumondji01@gmail.com>
2024-08-22 09:57:18 +0000

    Modification du 22/08

--- app/(home)/card/components/Header.tsx
+++ app/(home)/card/components/Header.tsx
@@ -264,14 +264,6 @@ const Header = () =>{
                   </div>
                 )}
               </li>
-              {/* <li>
-                <a
-                  href="/tarifs"
-                  className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#50c878] dark:text-white dark:hover:bg-gray-700 dark:hover:text-[#50c878] md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Tarifs
-                </a>
-              </li> */}
               <li>
                 <a
                   href="/contact"
