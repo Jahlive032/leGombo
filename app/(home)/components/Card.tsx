84a3e08bee618c287fea87946fe36cf484667188
Jahlive032 <erickoumondji01@gmail.com>
2024-09-14 23:17:39 +0000

    Modification du 14/09

--- app/(home)/components/Card.tsx
+++ app/(home)/components/Card.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import React from 'react'
 import { motion } from 'framer-motion'
 
-export const Card = ({ url, title, description, className }: { url: string, title?: string, description?: string, className?: string }) => {
+export const Card = ({ url, title, className }: { url: string, title?: string, className?: string }) => {
   return (
     <motion.div
       initial={{ scale: 0 }}
@@ -17,13 +17,13 @@ export const Card = ({ url, title, description, className }: { url: string, titl
     >
       {/* Image Section */}
       <div className='w-20 h-20 relative flex items-center justify-center p-2'>
-        <img src={url} alt={title} description={description} className='w-20  object-contain rounded-full' />
+        <img src={url} alt={title}  className='w-20  object-contain rounded-full' />
       </div>
 
       {/* Text Section */}
       <div className=''>
         {title && <h2 className='text-lg font-semibold'>{title}</h2>}
-        {description && <p className='text-sm text-gray-500'>{description}</p>}
+        {/* {description && <p className='text-sm text-gray-500'>{description}</p>} */}
       </div>
     </motion.div>
   )
